796b44c6f71bc12d778424565aeea7a50608bc4f
tmtuanyd <tmtuanyd@gmail.com>
2021-05-17 14:25:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
+import React, {useRef, useState, useCallback} from "react";
 import './App.css';
+import useBookSearch from "./UseBookSearch";
 
 function App() {
+    const [query, setQuery] = useState('')
+    const [pageNumber, setPageNumber] = useState('')
+    const {books, error, hasMore, loading} = useBookSearch(query, pageNumber)
+    const observer = useRef()
+    const lastBookElementRef = useCallback(node => {
+        if (loading) return
+        if (observer.current) observer.current.disconnect()
+        observer.current = new IntersectionObserver(entries => {
+            if(entries[0].isIntersecting && hasMore){
+                // console.log('Visible')
+                setPageNumber(prevPageNumber=>prevPageNumber+1)
+            }
+        })
+        if(node) observer.current.observe(node)
+    }, [loading, hasMore])
+    const handleSearch = (e)=>{
+        setQuery(e.target.value)
+        setPageNumber()
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <input type="text" value={query} onChange={handleSearch}/>
+          {books.map((book, index) => {
+              if(books.length === index + 1){
+                  return <div ref={lastBookElementRef} key={book}>{book}</div>
+              } else {
+                  return <div key={book}>{book}</div>
+              }
+          })}
+          <div>{loading && 'Loading ...'}</div>
+          <div>{error && 'Error'}</div>
+      </>
   );
 }
 
